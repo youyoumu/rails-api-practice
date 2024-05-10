bfc1baea7736a16f165421da2a9504315671d400
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-10 16:02:52 +0700

    Scaffold post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_10_081231) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_10_090113) do
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer "resource_owner_id", null: false
     t.integer "application_id", null: false
@@ -51,6 +51,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_10_081231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
